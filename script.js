744b4f192a6eaffe4dff9b18701c717f6a660f77
Calvin Schwob <calvin.schwob@gmail.com>
2023-11-12 00:07:59 +0100

    Completed the filling function for the pixels Finished the function to first remove the pixels and then set the pixels in an responsive grid automatically (fkn finally)

--- script.js
+++ script.js
@@ -3,15 +3,16 @@ let pixelContainer = document.querySelector(".pixelContainer");
 
 function setPixel (pixel) {
 
-    // clearPixelContainer ();
+    clearPixelContainer();
+    pixelContainer.style.gridTemplateColumns = `repeat(${pixel}, auto`;
+    pixelContainer.style.gridTemplateRows = `repeat(${pixel}, auto`;
 
     for (let i = 0; i < pixel * pixel; i++) {
         let pixel = document.createElement("div");
         pixel.style.border = "1px solid black";
-        pixel.style.minWidth = "10px";
-        pixel.style.minHeight = "10px";
-        pixel.style.aspectRatio = "1/1";
-        pixel.style.flexGrow = "1";
+        // pixel.style.minWidth = "10px";
+        // pixel.style.minHeight = "10px";
+        // // pixel.style.aspectRatio = "1/1";
 
         pixelContainer.appendChild(pixel);
     }
